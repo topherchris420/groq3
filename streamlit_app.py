32f4af745ba045c26bd71c3998803cd4377b23a0
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-18 22:35:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ system_prompt = _get_system_prompt()
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system", "content": system_prompt}]
 
-st.set_page_config(page_icon="👩🏽‍⚕️", layout="wide", page_title="GroqMind")
+st.set_page_config(page_icon="images/1920x1080bb-80.png", layout="wide", page_title="GroqMind")
 
 def icon(emoji: str):
     """Shows an emoji as a Notion-style page icon."""
