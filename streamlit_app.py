8aca1d3b4e8cb6485865ecf1ef44145f937116fb
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-19 09:34:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def icon(emoji: str):
     st.write(f'<span style="font-size: 78px; line-height: 1">{emoji}</span>', unsafe_allow_html=True)
 
 icon("🫂")
-st.markdown('<a href="https://visualverse.streamlit.app/" style="text-decoration:none; color: #00C6C3;"><h2>Vers3Dynamicss</h2></a>', unsafe_allow_html=True)
+st.markdown('<a href="https://visualverse.streamlit.app/" style="text-decoration:none; color: #00C6C3;"><h2>Vers3Dynamics</h2></a>', unsafe_allow_html=True)
 st.subheader("Meet Your Mental Health Companion, Powered by Groq 🌿")
 
 # Add a picture with a caption
